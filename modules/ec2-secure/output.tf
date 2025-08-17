1779e40ebaf74da52867e2b83a8f67eda59d1774
Elijah Johnson <97414090+ej-east@users.noreply.github.com>
2025-08-17 12:16:15 -0400

    Added outpuit of SG

--- modules/ec2-secure/output.tf
+++ modules/ec2-secure/output.tf
@@ -1,14 +1,19 @@
-output "public_ip" {
-  description = "The public IP of the ec2 instance"
+output "bastion_host_public_ip" {
+  description = "The public IP of the bastion_host"
   value       = aws_instance.bastion_host.public_ip
 }
 
-output "aws_instance_id" {
-  description = "The ID of the ec2 instance"
+output "bastion_host_id" {
+  description = "The ID of the bastion_host"
   value       = aws_instance.bastion_host.id
 }
 
-output "aws_instance_arn" {
-  description = "The ARN of the ec2 instance"
+output "bastion_host_arn" {
+  description = "The ARN of the bastion_host"
   value       = aws_instance.bastion_host.arn
+}
+
+output "bastion_host_security_group_id" {
+  description = "The security group ID of the bastion_host"
+  value       = aws_security_group.bastion_host.id
 }
